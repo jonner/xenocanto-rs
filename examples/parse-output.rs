2d0ef403ba823596a192da1327a9f3380ec2733a
Jonathon Jongsma <jonathon@quotidian.org>
2026-04-24 09:27:51 -0500

    Allow passing filename to parse-output example
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- examples/parse-output.rs
+++ examples/parse-output.rs
@@ -2,7 +2,10 @@ use std::io::Read;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
-    let mut f = std::fs::File::open("example-output")?;
+    let file = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| "example-output".into());
+    let mut f = std::fs::File::open(file)?;
     let mut response_text = String::new();
     f.read_to_string(&mut response_text)?;
     let response: xenocanto::recording::RecordingSet = serde_json::from_str(&response_text)?;
